1c44c2e9583e9a7634c6079b244891290e2df037
ali jahangiri <jahangiri.dev@gmail.com>
2021-01-26 22:12:11 +0330

    initial commit + add tailwind and Google Auth

---
+++ src/components/Auth.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./Login";
+
+const Auth = () => {
+  return (
+    <div className="">
+      <p>Login or Registr </p>
+      <Login />
+    </div>
+  );
+};
+
+export default Auth;
